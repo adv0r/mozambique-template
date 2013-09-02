fd0be962f19785b767cdea88472fb198d5b280f4
morgantar <danielemeli86@gmail.com>
2013-09-02 17:13:13 +0200

    Modificato ordine sections e metodo parallax

--- www/ita/scripts/parallax.js
+++ www/ita/scripts/parallax.js
@@ -1,14 +1,16 @@
-// JavaScript Document
-
 $(document).ready(function() {
 	var $window = $(window);
 	var $one = $('#one');
 	var $two = $('#two');
 	var $three = $('#three');
 	var $four = $('#four');
-	var cricket = $("#two .bg");
+	var $cricket = $("#two .bg");
+	var $video_pattern = $("#video_pattern");
+	var $page_height = $(".page_height");
 	
 	var windowHeight = $window.height();
+	$page_height.css({'height': windowHeight});
+	
 	
 	$('#one, #two, #three, #four').bind('inview', function (event, visible) {
 		if (visible == true) {
@@ -33,16 +35,14 @@ $(document).ready(function() {
 	function Parallax(){ 
 		var pos = $window.scrollTop(); 
 		if($one.hasClass("inview")){
-			$one.css({'backgroundPosition': movePos(50, windowHeight, pos, 900, 0.3)}); 
+			$one.css({'backgroundPosition': movePos(50, windowHeight, pos, windowHeight, 0.3)}); 
 		}
 		if($two.hasClass("inview")){
-			$two.css({'backgroundPosition': movePos(50, windowHeight, pos, 1250, 0.3)});
-			cricket.css({'backgroundPosition': movePos(30, windowHeight, pos, 1900, 0.6)});
+			$two.css({'backgroundPosition': movePos(50, windowHeight, pos, windowHeight*2, 0.3)}); /* TODO Questo snippet serve a muovere lo sfondo ad una velocità diversa rispetto allo scroll (0.3%). la sintassi "*4" significa "il quarto div" e "windowHeight" è l'altezza della finestra del browser */
 		}
 		if($three.hasClass("inview")){
-			$three.css({'backgroundPosition': movePos(50, windowHeight, pos, 2850, 0.3)});
+			$three.css({'backgroundPosition': movePos(50, windowHeight, pos, windowHeight*3, 0.3)});
 		}
-		
 		if($four.hasClass("inview")){
 			$four.css({'backgroundPosition': movePos(0, windowHeight, pos, 200, 0.9) + ", " + movePos(50, windowHeight, pos, 0, 0.7) + ", " + movePos(50, windowHeight, pos, 0, 0.5) + ", " + movePos(50, windowHeight, pos, 3700, 0.3)});
 		}

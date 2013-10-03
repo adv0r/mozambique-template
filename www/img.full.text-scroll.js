e09a2495fa32d448981620748799ff03907a6e35
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2013-10-03 12:39:19 +0200

    updated full.text.scroll. Partially works

--- www/img.full.text-scroll.js
+++ www/img.full.text-scroll.js
@@ -1,4 +1,6 @@
  var sectionContainer =  document.getElementById('section-container');
+ var textContainer =  document.getElementById('text-container');
+
  var textLength = 300  ; //The length (px) of the text to scroll
  var startScrollText =  sectionContainer.offsetTop;//at this height block the image and start scrolling text
  var stopScrollText = startScrollText+textLength; //ad this height stop scrolling text and go down
@@ -8,14 +10,19 @@
 $(window).scroll(function() { 
     currentScroll = getScrollTop(); //get the current position
      $('#scroll-pos').html(currentScroll+"px"); //update the view with the current height
-
      if(currentScroll>startScrollText && currentScroll < stopScrollText)
      {
             //Scroll down the text
+            textContainer.style.position='fixed';
             console.log(currentScroll +' Must be Scrolling the text');
+            scrollDivDown('text-container');
      }
-
+     else
+     {
+             textContainer.style.position='relative';
+     }
 });
+
 
 
 /* -  Get the position from top, cross browser -  */ 
@@ -31,3 +38,29 @@ function getScrollTop(){
         return D.scrollTop;
     }
 }
+
+/* Functions to control the scroll inside a div*/
+function toTop(id){ 
+    document.getElementById(id).scrollTop=0 
+} 
+
+defaultStep=1 
+step=defaultStep 
+
+function scrollDivDown(id){ 
+    document.getElementById(id).scrollTop+=step 
+    timerDown=setTimeout("scrollDivDown('"+id+"')",10) 
+} 
+
+function scrollDivUp(id){ 
+    document.getElementById(id).scrollTop-=step 
+    timerUp=setTimeout("scrollDivUp('"+id+"')",10) 
+} 
+
+function toBottom(id){ 
+    document.getElementById(id).scrollTop=document.getElementById(id).scrollHeight 
+} 
+
+function toPoint(id){ 
+    document.getElementById(id).scrollTop=100 
+} 

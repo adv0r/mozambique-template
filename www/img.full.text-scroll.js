a2f3aadfce246ba2d177df011c4bb67630f0d373
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2013-10-03 11:51:06 +0200

    First version of text-scroll. Added skrollr

--- www/img.full.text-scroll.js
+++ www/img.full.text-scroll.js
@@ -1,1 +1,17 @@
-nico-
+function getScrollTop(){
+    if(typeof pageYOffset!= 'undefined'){
+        //most browsers except IE before #9
+        return pageYOffset;
+    }
+    else{
+        var B= document.body; //IE 'quirks'
+        var D= document.documentElement; //IE with doctype
+        D= (D.clientHeight)? D: B;
+        return D.scrollTop;
+    }
+}
+
+$(window).scroll(function() { 
+    currentScroll = getScrollTop();
+     $('#scroll-pos').html(currentScroll+"px");
+});

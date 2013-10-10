1dfef28fa3634ff9fa41b7137bc394f50de295d7
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2013-10-10 14:35:52 +0200

    Added menu plugin

--- www/js/mozambique.js
+++ www/js/mozambique.js
@@ -1,19 +1,38 @@
 // Init skrollr
 function initSkrollr()
 {
-	skrollr.init({
+	var s=skrollr.init({
 	constants: {
 			container: 400,
 			container2: 50	
 		}
-	});	
+	});
+	initSkrollrMenu(s); //init the menu	
 }
 
-window.addEventListener('DOMContentLoaded', function() {	//This function is called on page load
+function initSkrollrMenu(s)
+{
+	skrollr.menu.init(s, {
+	//skrollr will smoothly animate to the new position using `animateTo`.
+	animate: true,
+	//The easing function to use.
+	easing: 'sqrt',
+	//How long the animation should take in ms.
+	duration: function(currentTop, targetTop) {
+		//By default, the duration is hardcoded at 500ms.
+		return 500;
+		//But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
+		//return Math.abs(currentTop - targetTop) * 10;
+		},
+	});
+}
 
-	initSkrollr();  //Comment if you UNCOMMENT the queryloader!
+window.addEventListener('DOMContentLoaded', function() {	//This function is called on page load, works on iOs
 
-	/* uncomment to see the preloader  and comment the line above
+	initSkrollr();  //Comment if you UNCOMMENT the queryloader below
+
+	//uncomment to see the preloader  and comment the line above
+	/* 
 	 $("body").queryLoader2(
 		{
 		        	percentage: true,

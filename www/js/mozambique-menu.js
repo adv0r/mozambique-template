841dcc8aeb06edc9be1168192e49da3d01528283
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2013-10-14 13:37:00 +0200

    Menu works good

--- www/js/mozambique-menu.js
+++ www/js/mozambique-menu.js
@@ -1,26 +1,41 @@
 var menuItem1, menuItem2,menuItem3, menuitem4, menuitem5;
 var menuItem6, menuItem7,menuItem8, menuitem9, menuitem10;
-var menuItem11, menuItem12,menuitem13,menuitem14,menuitem15,menuitem16;
+var menuItem11, menuItem12,menuitem13,menuitem14,menuitem15;
+var menuitem16,menuItem17,menuItem18, menuItem19;
 
 //To be able to use anchor we need to tell the page to scroll to the right position.  
 function updateMenuHrefHeights(viewportH)
 {	
-	menuItem1.setAttribute('data-menu-top', 0* viewportH); 
-	menuItem2.setAttribute( 'data-menu-top', 1* viewportH); 
-	menuItem3.setAttribute( 'data-menu-top', 2* viewportH); 
-	menuItem4.setAttribute( 'data-menu-top', 3* viewportH); 
-	menuItem5.setAttribute( 'data-menu-top', 4* viewportH); 
-	menuItem6.setAttribute( 'data-menu-top', 5* viewportH); 
-	menuItem7.setAttribute( 'data-menu-top', 6* viewportH); 
-	menuItem8.setAttribute( 'data-menu-top', 7* viewportH); 
-	menuItem9.setAttribute( 'data-menu-top', 8* viewportH); 
-	menuItem10.setAttribute( 'data-menu-top', 9* viewportH); 
-	menuItem11.setAttribute( 'data-menu-top', 10* viewportH); 
-	menuItem12.setAttribute( 'data-menu-top', 11* viewportH); 
-	menuItem13.setAttribute( 'data-menu-top', 12* viewportH);  	
-	menuItem14.setAttribute( 'data-menu-top', 13* viewportH);  	
-	menuItem15.setAttribute( 'data-menu-top', 14* viewportH);  		
-	menuItem16.setAttribute( 'data-menu-top', 15* viewportH);  		
+	var cumulativeLenght=0;
+	menuItem1.setAttribute('data-menu-top', 0* viewportH + cumulativeLenght); 
+	menuItem2.setAttribute( 'data-menu-top', 1* viewportH + cumulativeLenght); 
+	menuItem3.setAttribute( 'data-menu-top', 2* viewportH + cumulativeLenght); 
+	menuItem4.setAttribute( 'data-menu-top', 3* viewportH + cumulativeLenght); 
+	menuItem5.setAttribute( 'data-menu-top', 4* viewportH + cumulativeLenght); 
+	cumulativeLenght+=3850;
+	menuItem6.setAttribute( 'data-menu-top', 5* viewportH + cumulativeLenght); 
+	cumulativeLenght+=3900;
+	menuItem7.setAttribute( 'data-menu-top', 6* viewportH + cumulativeLenght); 
+	cumulativeLenght+=700;
+	menuItem8.setAttribute( 'data-menu-top', 7* viewportH + cumulativeLenght); 
+	cumulativeLenght+=4450;
+	menuItem9.setAttribute( 'data-menu-top', 8* viewportH + cumulativeLenght); 
+	menuItem10.setAttribute( 'data-menu-top', 9* viewportH + cumulativeLenght); 
+	menuItem11.setAttribute( 'data-menu-top', 10* viewportH + cumulativeLenght); 
+	menuItem12.setAttribute( 'data-menu-top', 11* viewportH + cumulativeLenght); 
+	menuItem13.setAttribute( 'data-menu-top', 12* viewportH + cumulativeLenght);  	
+	cumulativeLenght+=2500;	
+	menuItem14.setAttribute( 'data-menu-top', 13* viewportH + cumulativeLenght);  	
+	cumulativeLenght+=1230;	
+	menuItem15.setAttribute( 'data-menu-top', 14* viewportH + cumulativeLenght);  		
+	cumulativeLenght+=1420;		
+	menuItem16.setAttribute( 'data-menu-top', 15* viewportH + cumulativeLenght);  		
+	cumulativeLenght+=500;		
+	menuItem17.setAttribute( 'data-menu-top', 16* viewportH + cumulativeLenght);  		
+	cumulativeLenght+=5900;		
+	menuItem18.setAttribute( 'data-menu-top', 17* viewportH + cumulativeLenght);  		
+	cumulativeLenght+=2000;		
+	menuItem19.setAttribute( 'data-menu-top', 18* viewportH + cumulativeLenght);  		
 }
 
 
@@ -43,4 +58,8 @@ function initMenuItemVars()
 	menuItem14 = document.getElementById("#menu-item-14"); 
 	menuItem15 = document.getElementById("#menu-item-15"); 
 	menuItem16 = document.getElementById("#menu-item-16"); 
+	menuItem17 = document.getElementById("#menu-item-17"); 
+	menuItem18 = document.getElementById("#menu-item-18"); 
+	menuItem19 = document.getElementById("#menu-item-19"); 
+
 }

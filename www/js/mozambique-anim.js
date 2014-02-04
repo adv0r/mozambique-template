abd13cc14a288eef5dcadc08b358517db522cf6a
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2014-02-04 12:30:06 +0100

    refined slide04 still doesnt work

--- www/js/mozambique-anim.js
+++ www/js/mozambique-anim.js
@@ -109,7 +109,7 @@ function getPausing(sectionNumber,percentage) /*RALLENTA IL CONTENUTO DI UNA SEC
 		case 1: sectionPausingPx = 0; break;
 		case 2: sectionPausingPx = 0; break;
 		case 3: sectionPausingPx = viewportH*1.5 ; break;
-		case 4: sectionPausingPx = 0; break;
+		case 4: sectionPausingPx = viewportH + $('#slide04text').innerHeight(); break;
 		case 5: sectionPausingPx = 0; break; 
 		case 6: sectionPausingPx = 0; break; 
 		case 7: sectionPausingPx = 0; break;

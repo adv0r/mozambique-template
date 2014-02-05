b773d1ed69037cd09d0455cea75c796bbf30daf0
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2014-02-05 10:51:43 +0100

    Nuove immagini nuovi pausing

--- www/js/mozambique-anim.js
+++ www/js/mozambique-anim.js
@@ -110,10 +110,10 @@ function getPausing(sectionNumber,percentage) /*RALLENTA IL CONTENUTO DI UNA SEC
 		case 2: sectionPausingPx = viewportH/2; break;
 		case 3: sectionPausingPx = viewportH*3.8 ; break;
 		case 4: sectionPausingPx = viewportH*3 + $('#slide04text').innerHeight(); break;
-		case 5: sectionPausingPx = viewportH/2; break; 
+		case 5: sectionPausingPx = viewportH; break; 
 		case 6: sectionPausingPx = 0; break; 
-		case 7: sectionPausingPx = 0; break;
-		case 8: sectionPausingPx = 0; break; 
+		case 7: sectionPausingPx = viewportH; break;
+		case 8: sectionPausingPx =  viewportH*3 + $('#slide08textid').innerHeight(); break; 
 		case 9: sectionPausingPx = 0; break;
 		case 10: sectionPausingPx = 0; break;
 		case 11: sectionPausingPx = 0; break;

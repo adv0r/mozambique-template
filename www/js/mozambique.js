3e4dee737d878252dbb40080b974960c2907c70e
morgantar <danielemeli86@gmail.com>
2014-02-28 12:29:30 +0100

    finendo il redirect

--- www/js/mozambique.js
+++ www/js/mozambique.js
@@ -62,20 +62,17 @@ function preLoad()
 
 function checkMobile()
 {
-
-	$(window).load(function(){
-		if( (navigator.userAgent.match(/Android/i)) ||
-			(navigator.userAgent.match(/webOS/i)) ||
-			(navigator.userAgent.match(/iPhone/i)) ||
-			(navigator.userAgent.match(/iPad/i)) ||
-			(navigator.userAgent.match(/iPod/i)) ||
-			(navigator.userAgent.match(/BlackBerry/i)) ||
-			(navigator.userAgent.match(/IEMobile/i)) ||
-			(navigator.userAgent.match(/Opera Mini/i)))
-		{
-			location.replace("mobile.html");
-		}
-	});
+	if( (navigator.userAgent.match(/Android/i)) ||
+		(navigator.userAgent.match(/webOS/i)) ||
+		(navigator.userAgent.match(/iPhone/i)) ||
+		(navigator.userAgent.match(/iPad/i)) ||
+		(navigator.userAgent.match(/iPod/i)) ||
+		(navigator.userAgent.match(/BlackBerry/i)) ||
+		(navigator.userAgent.match(/IEMobile/i)) ||
+		(navigator.userAgent.match(/Opera Mini/i)))
+	{
+		location.replace("mobile.html");
+	}
 }
 
 
